f7d53105d4a10c5bfa75250b43772710391bb185
Giang2003 <89984724+Giang2003@users.noreply.github.com>
2022-02-28 23:45:28 +0700

    Add files via upload

---
+++ dp_01.py
@@ -0,0 +1,22 @@
+def intro():
+    print("Go, team go!")
+    print("We can do it.")
+
+def verses():
+    print("Go, team go!")
+    print("We can do it.")
+    print("We're the 'cats,")
+    print("We'll leave it at that.")
+    print("Go, team go!")
+    print("We can do it.")
+
+def print_fight_song():
+    intro()
+    print()
+    verses()
+    print()
+    verses()
+    print()
+    intro()
+
+print_fight_song()
